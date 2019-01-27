bee9bbf8d31d12ae4f4b77ea6dad69d22f1781c6
etomsky <etomsy@yahoo.com>
2019-01-27 14:08:22 -0500

    Initial Scripting

---
+++ I'll Be Home After Christmas/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+    }
+	
+	void LateUpdate () {
+        Vector3 tempPOS;
+        tempPOS = player.transform.position;
+        tempPOS.y = this.transform.position.y;
+        tempPOS.z = this.transform.position.z;
+        transform.position = tempPOS;
+    }
+}
--- I'll Be Home After Christmas/Assets/Scripts/SantaController.cs
+++ I'll Be Home After Christmas/Assets/Scripts/SantaController.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class SantaController : MonoBehaviour {
 
+    private Rigidbody2D rb2d;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb2d = this.gameObject.GetComponent<Rigidbody2D>();
+        //rb2d.velocity = new Vector2 (5.0f, 0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //rb2d.AddForce(new Vector2(5.0f, 0.0f));
+        rb2d.velocity = new Vector2(5.0f, 0.0f);
+
+        float upForce = Input.GetAxis("Vertical");
+        rb2d.AddForce(new Vector2(0.0f, upForce * 50.0f));
+    }
 }
